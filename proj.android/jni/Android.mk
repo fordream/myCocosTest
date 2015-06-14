79ceaf97b2617d7b8a3b62d6a355056b52a3ac6a
kevin <mac@MacdeMacBook-Pro.local>
2015-06-14 18:14:12 +0800

    Android.mk

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -10,11 +10,24 @@ LOCAL_MODULE := cocos2dcpp_shared
 
 LOCAL_MODULE_FILENAME := libcocos2dcpp
 
-LOCAL_SRC_FILES := hellocpp/main.cpp \
-                   ../../Classes/AppDelegate.cpp \
-                   ../../Classes/HelloWorldScene.cpp
+# 遍历目录及子目录的函数
+define walk
+    $(wildcard $(1)) $(foreach e, $(wildcard $(1)/*), $(call walk, $(e)))
+endef
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+# 遍历Classes目录和子目录文件，并存入ALLFILES变量中
+ALLFILES = $(call walk, $(LOCAL_PATH)/../../Classes)
+
+FILE_LIST := hellocpp/main.cpp
+# 从ALLFILES中提取出所有.cpp和.c文件，并存入FILE_LIST中
+FILE_LIST += $(filter %.cpp, $(ALLFILES))
+FILE_LIST += $(filter %.c, $(ALLFILES))
+
+LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
+
+FILE_INCLUDES := $(shell find $(LOCAL_PATH)/../../Classes -type d)
+LOCAL_C_INCLUDES := $(FILE_INCLUDES)
+
 
 # _COCOS_HEADER_ANDROID_BEGIN
 # _COCOS_HEADER_ANDROID_END
